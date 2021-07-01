e3b61b98cadffa0614708453bb4a4204bccebe7e
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-07-01 17:21:15 +0300

    Resolve the Decidim source path with @snyk/gemfile
    Change the Decidim source path resolving to use @snyk/gemfile
    instead of running `bundle` in a child process. Running `bundle`
    won't work when ruby/bundler is not yet installed.
    
    This allows resolving the Decidim source path from the
    `Gemfile.lock` of the project when Decidim is installed either
    from git or from a file path.

--- bin/decidimpack.js
+++ bin/decidimpack.js
@@ -1,29 +1,22 @@
 #!/usr/bin/env node
 
-const { exec, execSync } = require("child_process");
 const createBuilds = require("../src/create-builds");
+const resolveSource = require("../src/resolve-source");
+const path = require("path");
 
-const projectPath = process.argv[2];
+let projectPath = process.argv[2];
 if (!projectPath) {
   throw new Error("Please provide the project path as the first argument!");
 }
 
-const resolveSource = () => {
-  return new Promise((resolve, _reject) => {
-    exec("bundle show decidim", { stdio: "pipe" }, (error, stdout) => {
-      if (error) {
-        resolve("https://github.com/decidim/decidim/packages#develop");
-      } else {
-        resolve(`${stdout.trim()}/packages`);
-      }
-    });
-  });
-}
+// Normalize the path to remove any relative references
+projectPath = path.resolve(projectPath);
 
 const buildDir = `${projectPath}/tmp/npmbuild`;
 
-resolveSource().then((source) => {
+resolveSource(projectPath).then((source) => {
   console.log("Creating Decidim NPM builds...");
+  console.log(`  source: ${source}`);
   createBuilds(source, buildDir).then((packages) => {
     console.log("Builds created:");
     packages.forEach((pkg) => {
---
+++ src/resolve-source.js
@@ -0,0 +1,43 @@
+const { parse: parseGemfile } = require("@snyk/gemfile");
+const path = require("path");
+
+const resolveGemfileSource = (projectPath) => {
+  return new Promise((resolve, reject) => {
+    parseGemfile(`${projectPath}/Gemfile.lock`).then((gemfile) => {
+      for (const [type, defs] of Object.entries(gemfile)) {
+        if (type.match(/GIT[0-9]*/)) {
+          if (defs.specs["decidim"]) {
+            resolve(`${defs.remote.path}/packages#${defs.revision.sha}`);
+            return;
+          }
+        } else if (type.match(/PATH[0-9]*/)) {
+          if (defs.specs["decidim"]) {
+            let gemPath = defs.remote.path;
+            if (!gemPath) {
+              continue;
+            }
+            if (!gemPath.match(/^\//)) {
+              gemPath = path.resolve(projectPath, gemPath);
+            }
+            if (gemPath) {
+              resolve(`${gemPath}/packages`);
+              return;
+            }
+          }
+        }
+      }
+    }, reject);
+  });
+};
+
+module.exports = (projectPath) => {
+  return new Promise((resolve, _reject) => {
+    resolveGemfileSource(projectPath).then(
+      (source) => resolve(source),
+      () => {
+        // Default to Decidim git repository
+        resolve("https://github.com/decidim/decidim/packages#develop");
+      }
+    );
+  });
+}
