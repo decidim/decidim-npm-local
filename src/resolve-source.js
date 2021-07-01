816349491c1ff50b398b34c8ba8ab29b59096b40
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-07-01 17:32:53 +0300

    Improve the regexp matcher

--- src/resolve-source.js
+++ src/resolve-source.js
@@ -5,12 +5,12 @@ const resolveGemfileSource = (projectPath) => {
   return new Promise((resolve, reject) => {
     parseGemfile(`${projectPath}/Gemfile.lock`).then((gemfile) => {
       for (const [type, defs] of Object.entries(gemfile)) {
-        if (type.match(/GIT[0-9]*/)) {
+        if (type.match(/^GIT[0-9]*/)) {
           if (defs.specs["decidim"]) {
             resolve(`${defs.remote.path}/packages#${defs.revision.sha}`);
             return;
           }
-        } else if (type.match(/PATH[0-9]*/)) {
+        } else if (type.match(/^PATH[0-9]*/)) {
           if (defs.specs["decidim"]) {
             let gemPath = defs.remote.path;
             if (!gemPath) {
